765187bf5904f2e7a92d3e8655e65c6b9a073e69
Philippe Lagadec <decalage@laposte.net>
2014-09-26 06:17:12 +0200

    updated __init__.py

--- olefile/__init__.py
+++ olefile/__init__.py
@@ -1,4 +1,6 @@
 try:
+    # first try to import olefile for Python 2.6+/3.x
     from olefile import *
 except:
+    # if it fails, fallback to the old version olefile2 for Python 2.x:
     from olefile2 import *
