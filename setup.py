e948985d563a89ea8471a4426e1cfcc0fff95887
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-03 20:16:14 -0800

    Drop testing and official support for EOL Python 3.3
    Python 3.3 is end of life. It is no longer receiving bug fixes,
    including for security issues. It has been EOL since 2017-09-29.
    
    https://devguide.python.org/#status-of-python-branches

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ classifiers=[
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
@@ -241,6 +240,7 @@ def main():
         download_url=download_url,
 #        data_files=data_files,
 #        scripts=scripts,
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     )
 
 
