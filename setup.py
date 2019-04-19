6778c9985fb884b5462750a6bf58c0b8fe9fd399
Hugo <hugovk@users.noreply.github.com>
2019-04-19 21:43:35 +0300

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ classifiers = [
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
@@ -75,7 +74,7 @@ def main():
         download_url=download_url,
 #        data_files=data_files,
 #        scripts=scripts,
-        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     )
 
 
